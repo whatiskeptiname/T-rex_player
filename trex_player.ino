7db24c7205b5cdf5d07110db5ceeec94f3adcaf3
susang <susangmree@gmail.com>
2021-05-30 01:34:58 +0545

    Code refactoring and added Working

--- trex_player.ino
+++ trex_player.ino
@@ -1,22 +1,26 @@
+#define relay_pin 13 //initializing pin-13 as relay_pin 
+
 void setup() 
-{//setup 12 for output and 9600 baud for serial communication with PC
+{//setup relay_pin for output and 9600 baud for serial communication with PC
   Serial.begin(9600);
-  pinMode(12,OUTPUT);
+  pinMode(relay_pin, OUTPUT);
 }
 void loop()
 {
-  long data;
-  data=analogRead(A0); //reads the sensor data from A0 analog pin, there is no need to setup analog pin in setup()
-  Serial.println(data); //serial output the data in monitor
-  if(data<200)
+  int brightness;
+
+  brightness=analogRead(A0); //reads the sensor data from A0 analog pin, there is no need to setup analog pin in setup()
+  Serial.println(brightness); //serial output the data in monitor
+  
+  if(brightness<200)
   {//checks if the brightness value is less then 200 units highest value is 1023 due to 10 bit ADC
-  digitalWrite(12,HIGH);
-  //if the value is less then 200 (obstracle detected) activate the relay to simulate touch
+    digitalWrite(relay_pin, HIGH);
+    //if the value is less then 200 (obstracle detected) activate the relay to simulate touch
   }
   else
   {
-  digitalWrite(12,LOW);
-  //else the relay is inactivated
+    digitalWrite(relay_pin, LOW);
+    //else the relay is inactivated
   }
 }
 
